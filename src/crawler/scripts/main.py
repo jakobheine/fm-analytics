17f07a37e639c7694ebf0d80f2d5dc9e16285738
Jakob Heine <s0558639@htw-berlin.de>
2021-04-25 20:02:46 +0200

    Put crawler into production mode, add logging

--- src/crawler/scripts/main.py
+++ src/crawler/scripts/main.py
@@ -1,11 +1,12 @@
 import schedule
 import time
 from spitch import crawl_spitch_data
+import logging
+
+logging.info("Crawler up and running, waiting for scheduled event.")
 
 # execute the crawler every Monday 9:59PM UTC, which is 11:59PM MESZ
-# schedule.every().monday.at("21:59").do(crawl_spitch_data())
-
-crawl_spitch_data()
+schedule.every().monday.at("21:59").do(crawl_spitch_data)
 
 while True:
     schedule.run_pending()
