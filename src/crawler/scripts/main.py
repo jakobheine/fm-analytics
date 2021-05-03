ae24dac946890e7fe2a103bd26ce78cc730b8f83
Jakob Heine <s0558639@htw-berlin.de>
2021-05-03 16:08:21 +0200

    Change log

--- src/crawler/scripts/main.py
+++ src/crawler/scripts/main.py
@@ -3,7 +3,7 @@ import time
 from spitch import crawl_spitch_data
 import logging
 
-logging.info("Crawler up and running, waiting for scheduled event.")
+logging.info("Crawler up and running, waiting for scheduled event...")
 
 # execute the crawler every Monday 9:59PM UTC, which is 11:59PM MESZ
 schedule.every().monday.at("21:59").do(crawl_spitch_data)
